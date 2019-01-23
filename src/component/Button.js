3da8dc7db6c769316400127ddb61ed21e1327dfc
Minmin Tian <mtian@gwadmins-MBP-2.guidewire.com>
2019-01-23 18:54:33 -0500

    Fixed some minor issues.

--- src/component/Button.js
+++ src/component/Button.js
@@ -4,25 +4,33 @@ import '../assets/styles/_button.scss';
 class Button extends Component {
   render() {
     return (
-        
+    //  Uncomment the code to display different size.
+    //  Need to hover and click the button to show different styles for each state.
       <div className="button-section">
         <h1>This is the Business Theme Button.</h1>
-        <button className="button-business-primary lg"><span className="button-text">Primary</span></button>
         <div className="button-row">
             <p>Primary Button</p>
-            <button className="button-business-primary"><span className="button-text">Primary</span></button>
+            <button className="button-business-primary lg"><span className="button-text">Primary</span></button>
+            {/* <button className="button-business-primary md"><span className="button-text">Primary</span></button>
+            <button className="button-business-primary sm"><span className="button-text">Primary</span></button> */}
         </div>
         <div className="button-row">
-            <p>Primary Button <br/>Disabled</p>
-            <button disabled={true} className="button-business-primary"><span className="button-text">Primary Primary Primary</span></button>
+            <p>Primary Button Disabled</p>
+            <button disabled={true} className="button-business-primary lg"><span className="button-text">Primary</span></button>
+            {/* <button disabled={true} className="button-business-primary md"><span className="button-text">Primary</span></button>
+            <button disabled={true} className="button-business-primary sm"><span className="button-text">Primary</span></button> */}
         </div>
         <div className="button-row">
             <p>Secondary Button</p>
-            <button className="button-business-sec"><span className="button-text">Secondary</span></button>
+            <button className="button-business-sec lg"><span className="button-text">Secondary</span></button>
+            {/* <button className="button-business-sec md"><span className="button-text">Secondary</span></button>
+            <button className="button-business-sec sm"><span className="button-text">Secondary</span></button> */}
         </div>
         <div className="button-row">
-            <p>Secondary Button <br/>Disabled</p>
-            <button disabled={true} className="button-business-sec"><span className="button-text">Secondary</span></button>
+            <p>Secondary Button Disabled</p>
+            <button disabled={true} className="button-business-sec lg"><span className="button-text">Secondary</span></button>
+            {/* <button disabled={true} className="button-business-sec md"><span className="button-text">Secondary</span></button>
+            <button disabled={true} className="button-business-sec sm"><span className="button-text">Secondary</span></button> */}
         </div>
         
         <h1>This is the Consumer Theme Button.</h1>
@@ -32,7 +40,7 @@ class Button extends Component {
         </div>
          
         <div className="button-row">
-            <p>Primary Button <br/>Disabled</p>
+            <p>Primary Button Disabled</p>
             <button disabled={true} className="button-consumer-primary"><span className="button-text">Primary</span></button>
         </div>
        
