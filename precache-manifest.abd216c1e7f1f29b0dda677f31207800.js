a945a3b927f1d4dc748a43679f2447b2cc639ff3
Minmin Tian <mtian@gwadmins-MBP-2.guidewire.com>
2019-01-24 16:52:00 -0500

    Updates

---
+++ precache-manifest.abd216c1e7f1f29b0dda677f31207800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "774c05ccae92d11b95a2ef722c9566a8",
+    "url": "/my-button/static/media/SourceSansPro-SemiBold.774c05cc.ttf"
+  },
+  {
+    "revision": "bcc9c599783e0b28862b",
+    "url": "/my-button/static/js/runtime~main.bcc9c599.js"
+  },
+  {
+    "revision": "c27c9e97822ae0e63f9b",
+    "url": "/my-button/static/js/main.c27c9e97.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/my-button/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "c27c9e97822ae0e63f9b",
+    "url": "/my-button/static/css/main.bafaa2e9.chunk.css"
+  },
+  {
+    "revision": "5931e49002a35d8b7790201c8f0ac15f",
+    "url": "/my-button/index.html"
+  }
+];
